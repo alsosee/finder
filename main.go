4ea2f93d7b8db2fb03c98c36a9f6f7431565e3f8
Konstantin Chukhlomin <mail@chuhlomin.com>
2023-11-30 01:14:56 -0500

    feat: add GitHub Action

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ type Config struct {
 	InfoDirectory      string `env:"INPUT_INFO" short:"i" long:"info" description:"Directory that contains info files" default:"info"`
 	IgnoreFile         string `env:"INPUT_IGNOREFILE" short:"f" long:"ignorefile" description:"File that contains ignore patterns" default:".ignore"`
 	TemplatesDirectory string `env:"INPUT_TEMPLATES" short:"t" long:"templates" description:"Directory that contains templates" default:"templates"`
-	OutputDirectory    string `env:"INPUT_OUTPUT" short:"o" long:"output" description:"Directory to output generated files" default:"output"`
+	OutputDirectory    string `env:"INPUT_OUTPUT" short:"o" long:"output" description:"Directory to output static site" default:"output"`
 	NumWorkers         int    `env:"INPUT_NUMWORKERS" short:"w" long:"workers" description:"Number of workers to use" default:"4"`
 }
 
