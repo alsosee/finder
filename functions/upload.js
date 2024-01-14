691af2f57d6d93dcc84ed7845dfe9fcc2830cf4d
Konstantin Chukhlomin <mail@chuhlomin.com>
2024-01-14 17:32:55 -0500

    fix(uploader): work with larger files

--- functions/upload.js
+++ functions/upload.js
@@ -4,17 +4,30 @@ export async function onRequest(context) {
       case 'PUT':
         const key = context.request.headers.get("x-file-name");
         await context.env.MEDIA.put(key, context.request.body);
-        return new Response(`Put ${key} successfully!`);
+        return new Response(
+          JSON.stringify({ status: "ok", key: key }),
+          { status: 200 }
+        );
 
       default:
-        return new Response(`${context.request.method} is not allowed.`, {
-          status: 405,
-          headers: {
-            Allow: 'PUT',
-          },
-        });
+        return new Response(
+          JSON.stringify({
+            error: {
+              message: 'Method Not Allowed',
+            }
+          }),
+          {
+            status: 405,
+            headers: {
+              Allow: 'PUT',
+            },
+          }
+        );
     }
   } catch (err) {
-    return new Response(err.stack || err, { status: 500 });
+    return new Response(
+      JSON.stringify({ error: err.stack || err }),
+      { status: 500 }
+    );
   }
 }
