d885c3e933b8741a12a907f49010935b61225a8c
Konstantin Chukhlomin <mail@chuhlomin.com>
2024-01-17 00:42:18 -0500

    feat(upload): trigger GHA pull workflow on media

--- functions/upload.js
+++ functions/upload.js
@@ -3,7 +3,32 @@ export async function onRequest(context) {
     switch (context.request.method) {
       case 'PUT':
         const key = context.request.headers.get("x-file-name");
+
         await context.env.MEDIA.put(key, context.request.body);
+
+        const response = await fetch("https://api.github.com/repos/alsosee/media/dispatches", {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${context.env.GHP_TOKEN}`,
+            "Accept": "application/vnd.github.everest-preview+json",
+            "Content-Type": "application/json",
+            "User-Agent": "alsosee/finder/1.0.0 (CloudFlare Pages Function)"
+          },
+          body: JSON.stringify({
+            event_type: "pull",
+            client_payload: {
+              path: key,
+            },
+          }),
+        });
+
+        if (response.status !== 204) {
+          const text = await response.text();
+          console.log(response.headers);
+          console.log(text);
+          throw new Error(`GitHub API failed with status code ${response.status}`);
+        }
+
         return new Response(
           JSON.stringify({ status: "ok", key: key }),
           { status: 200 }
