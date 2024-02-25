28f39ddf8897d99b6016935e2097b2c75e1eaf4c
Konstantin Chukhlomin <mail@chuhlomin.com>
2024-02-25 10:45:19 -0500

    feat: use local r2uploader for "upload" pages function

--- functions/upload.js
+++ functions/upload.js
@@ -1,64 +1,86 @@
 export async function onRequest(context) {
   try {
     switch (context.request.method) {
-      case 'PUT':
-        const key = decodeURIComponent(context.request.headers.get("x-file-name"));
+      case "PUT":
+        const key = decodeURIComponent(
+          context.request.headers.get("x-file-name"),
+        );
         if (!key) {
           return new Response(
             JSON.stringify({ error: "Missing x-file-name header" }),
-            { status: 400 }
+            { status: 400 },
           );
         }
 
-        await context.env.MEDIA.put(key, context.request.body);
-
-        const response = await fetch("https://api.github.com/repos/alsosee/media/dispatches", {
-          method: "POST",
-          headers: {
-            "Authorization": `Bearer ${context.env.GHP_TOKEN}`,
-            "Accept": "application/vnd.github.everest-preview+json",
-            "Content-Type": "application/json",
-            "User-Agent": "alsosee/finder/1.0.0 (CloudFlare Pages Function)"
-          },
-          body: JSON.stringify({
-            event_type: "pull",
-            client_payload: {
-              path: key,
+        if (context.env.MEDIA) {
+          await context.env.MEDIA.put(key, context.request.body);
+        } else {
+          // send a POST request with body to local server
+          var uploaderResponse = await fetch("http://localhost:8789/upload", {
+            method: "POST",
+            body: context.request.body,
+            headers: {
+              "x-file-name": key,
             },
-          }),
-        });
+          });
+          if (uploaderResponse.status !== 201) {
+            const text = await response.text();
+            throw new Error(
+              `Local server failed with status code ${response.status}: ${text}`,
+            );
+          }
+        }
+
+        const response = await fetch(
+          "https://api.github.com/repos/alsosee/media/dispatches",
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${context.env.GHP_TOKEN}`,
+              Accept: "application/vnd.github.everest-preview+json",
+              "Content-Type": "application/json",
+              "User-Agent": "alsosee/finder/1.0.0 (CloudFlare Pages Function)",
+            },
+            body: JSON.stringify({
+              event_type: "pull",
+              client_payload: {
+                path: key,
+              },
+            }),
+          },
+        );
 
         if (response.status !== 204) {
           const text = await response.text();
           console.log(response.headers);
           console.log(text);
-          throw new Error(`GitHub API failed with status code ${response.status}`);
+          throw new Error(
+            `GitHub API failed with status code ${response.status}`,
+          );
         }
 
-        return new Response(
-          JSON.stringify({ status: "ok", key: key }),
-          { status: 200 }
-        );
+        return new Response(JSON.stringify({ status: "ok", key: key }), {
+          status: 200,
+        });
 
       default:
         return new Response(
           JSON.stringify({
             error: {
-              message: 'Method Not Allowed',
-            }
+              message: "Method Not Allowed",
+            },
           }),
           {
             status: 405,
             headers: {
-              Allow: 'PUT',
+              Allow: "PUT",
             },
-          }
+          },
         );
     }
   } catch (err) {
-    return new Response(
-      JSON.stringify({ error: err.stack || err }),
-      { status: 500 }
-    );
+    return new Response(JSON.stringify({ error: err.stack || err }), {
+      status: 500,
+    });
   }
 }
