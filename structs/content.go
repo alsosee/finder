da3f10e3f4aa27f6642badb0a22163b142f55976
Konstantin Chukhlomin <mail@chuhlomin.com>
2024-06-19 00:08:54 -0400

    fix: add Cinematography and Teleplay fields to Episode

--- structs/content.go
+++ structs/content.go
@@ -233,16 +233,18 @@ type Winner struct {
 }
 
 type Episode struct {
-	Name        string
-	Description string        `yaml:",omitempty" json:",omitempty"`
-	Length      time.Duration `yaml:",omitempty" json:",omitempty"`
-	Released    string        `yaml:",omitempty" json:",omitempty"`
-	Directors   oneOrMany     `yaml:",omitempty" json:",omitempty"`
-	Writers     oneOrMany     `yaml:",omitempty" json:",omitempty"`
-	Editors     oneOrMany     `yaml:",omitempty" json:",omitempty"`
-	Story       oneOrMany     `yaml:",omitempty" json:",omitempty"`
-	Studio      string        `yaml:",omitempty" json:",omitempty"`
-	Characters  []*Character  `yaml:",omitempty" json:",omitempty"`
+	Name           string
+	Description    string        `yaml:",omitempty" json:",omitempty"`
+	Length         time.Duration `yaml:",omitempty" json:",omitempty"`
+	Released       string        `yaml:",omitempty" json:",omitempty"`
+	Directors      oneOrMany     `yaml:",omitempty" json:",omitempty"`
+	Writers        oneOrMany     `yaml:",omitempty" json:",omitempty"`
+	Editors        oneOrMany     `yaml:",omitempty" json:",omitempty"`
+	Cinematography oneOrMany     `yaml:",omitempty" json:",omitempty"`
+	Teleplay       oneOrMany     `yaml:",omitempty" json:",omitempty"`
+	Story          oneOrMany     `yaml:",omitempty" json:",omitempty"`
+	Studio         string        `yaml:",omitempty" json:",omitempty"`
+	Characters     []*Character  `yaml:",omitempty" json:",omitempty"`
 
 	IMDB      string `yaml:",omitempty" json:",omitempty"`
 	TMDB      string `yaml:",omitempty" json:",omitempty"`
